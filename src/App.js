cf55f56d7fd7b004bd3565a413079fcf23d4435b
TaniaAlex <2tatyanan@gmail.com>
2020-08-10 22:43:44 +0200

    Add funk to handle search

--- src/App.js
+++ src/App.js
@@ -4,14 +4,26 @@ import useFetchJobs from "./useFetchJobs";
 import { Container } from "react-bootstrap";
 import Job from "./Job";
 import JobsPage from "./JobsPage";
+import SearchJobForm from "./SearchJobForm";
 
 function App() {
   const [params, setParams] = useState({});
   const [page, setPage] = useState(1);
   const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
+
+  function handleParamChange(e) {
+    const param = e.target.name;
+    const val = e.target.val;
+    setPage(1);
+    setParams((prevParams) => {
+      return { ...prevParams, [param]: val };
+    });
+  }
+
   return (
     <Container className="my-4">
-      <h1 className="mb-4">Jobs GitHub</h1>
+      <h1 className="mb-4">Job Search</h1>
+      <SearchJobForm params={params} onParamChange={handleParamChange} />
       <JobsPage page={page} setPage={setPage} hasNextPage={hasNextPage} />
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error...Refresh Page...</h1>}
---
+++ src/SearchJobForm.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SearchJobForm({ params, onParamChange }) {
+  return <div>it job search</div>;
+}
